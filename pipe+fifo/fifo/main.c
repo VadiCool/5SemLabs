6bd46ccef7ae7d3992e4c91e2f82f382848d3210
Николашин Вадим <vadicool02@bk.ru>
2023-12-15 18:39:21 +0300

    Added pipe+fifo

---
+++ pipe+fifo/fifo/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <string.h>
+
+int main() {
+	
+	if (mkfifo("fifoFile", 0777) == -1) {
+		if (errno != EEXIST) {
+			printf("Cant create fifo\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	
+	int id = fork();
+	if (id == -1) {
+		printf("Fork error\n");
+		exit(EXIT_FAILURE);
+	}
+	if (id != 0) {
+
+		char str[100];
+		strcat(str, "Hello word!\n");
+		strcat(str, "id: ");
+
+		char strId[10];
+		sprintf(strId, "%i", id);
+
+		strcat(str, strId);
+		strcat(str, "\n");
+
+		time_t mytime = time(NULL);
+		char *curTime = ctime(&mytime);
+		strcat(str, curTime);
+
+		sleep(5);
+
+		int fd = open("fifoFile", O_WRONLY);
+		if (fd == -1) {
+			printf("Cant open file\n");
+			exit(EXIT_FAILURE);
+		}
+
+		write(fd, str, 100);
+		close(fd);
+
+
+	}
+	else {
+		int fd = open("fifoFile", O_RDONLY);
+		if (fd == -1) {
+			printf("Cant open file\n");
+			exit(EXIT_FAILURE);
+		}
+
+		char str[100];
+		read(fd, str, 100);
+		close(fd);
+		printf("%s", str);
+		
+	}
+	
+	return 0;
+}
---
+++ pipe+fifo/pipe/main.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+	int fd[2];
+
+	if(pipe(fd) == -1) {
+		printf("Error\n");
+		return -1;
+	}
+
+	int id = fork();
+	if (id == -1) {
+		printf("Fork errror\n");
+		return -1;
+	}
+	if (id != 0) {
+		close(fd[0]);
+
+		//char str[] = "Hello word!\n";
+		char *str = malloc(100);
+		strcat(str, "Hello word!\n");
+		strcat(str, "id: ");
+
+		char strId[10];
+		sprintf(strId, "%i", id);
+
+		strcat(str, strId);
+		strcat(str, "\n");
+
+		time_t mytime = time(NULL);
+		char *curTime = ctime(&mytime);
+		strcat(str, curTime);
+
+		sleep(5);
+
+		write(fd[1], str, 100);
+		close(fd[1]);
+	} else {
+		close(fd[1]);
+		char str[100];
+		read(fd[0], str, 100);
+		close(fd[0]);
+		printf("%s", str);
+	}
+
+
+
+	return 0;
+}
